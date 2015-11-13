72125f9fb78fca063a5b5f7c2cb51e433952a364
andrewdahm <andrew.dahm@gmail.com>
2015-11-13 09:35:26 -0600

    Added base project.

---
+++ SiHomework/app/src/main/java/com/sportinginnovations/sihomework/EventsActivity.java
@@ -0,0 +1,13 @@
+package com.sportinginnovations.sihomework;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+public class EventsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.events_activity);
+    }
+}
