00eed155d9edc1aef4608d3943704606e3054c38
jdvoss <jvoss@sportinginnovations.com>
2015-11-17 15:54:05 -0600

    Added base project.

---
+++ SIHomework/SIHomework/Source/SIViewController.m
@@ -0,0 +1,19 @@
+//
+//  ViewController.m
+//
+//  Copyright © 2015 Sporting Innovations. All rights reserved.
+//
+
+#import "SIViewController.h"
+
+@interface SIViewController ()
+
+@end
+
+@implementation SIViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+@end
