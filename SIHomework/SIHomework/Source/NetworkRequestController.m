0ff88686ed9244088ffe3f648f696782dcda7a49
joe DeCapo <joe@polka.cat>
2015-11-25 12:22:32 -0600

    Add controller class for network requests

---
+++ SIHomework/SIHomework/Source/NetworkRequestController.m
@@ -0,0 +1,13 @@
+//
+//  NetworkRequestController.m
+//  SIHomework
+//
+//  Created by goodle on 11/25/15.
+//  Copyright © 2015 Sporting Innovations. All rights reserved.
+//
+
+#import "NetworkRequestController.h"
+
+@implementation NetworkRequestController
+
+@end
