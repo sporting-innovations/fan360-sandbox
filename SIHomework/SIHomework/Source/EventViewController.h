47623f8b0ab6b73db1bf27d5f2cb6cc0ddcbebde
joe DeCapo <joe@polka.cat>
2015-11-27 23:34:13 -0600

    Add EventViewController class

---
+++ SIHomework/SIHomework/Source/EventViewController.h
@@ -0,0 +1,13 @@
+//
+//  EventViewController.h
+//  SIHomework
+//
+//  Created by goodle on 11/27/15.
+//  Copyright © 2015 Sporting Innovations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventViewController : UIViewController
+
+@end
